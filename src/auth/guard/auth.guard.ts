ad92dca38ceca3ace181e9d831a67896ab59eee3
Matias Galeano <99203645+matigaleanodev@users.noreply.github.com>
2025-03-20 17:06:08 -0300

    feat(auth): implementación de guard de autenticación en API Gateway

---
+++ src/auth/guard/auth.guard.ts
@@ -0,0 +1,49 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  Inject,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { ClientProxy } from '@nestjs/microservices';
+import { lastValueFrom } from 'rxjs';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+  constructor(
+    @Inject('AUTH_SERVICE') private readonly authClient: ClientProxy,
+  ) {}
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const request = context
+      .switchToHttp()
+      .getRequest<
+        Request & { email?: string; headers: { authorization?: string } }
+      >();
+    const authHeader = request.headers.authorization;
+
+    if (!authHeader) {
+      throw new UnauthorizedException('Token no encontrado');
+    }
+
+    const token = authHeader.split(' ')[1];
+    if (!token) {
+      throw new UnauthorizedException('Formato de token inválido');
+    }
+
+    try {
+      const response: { isValid: boolean; email: string } = await lastValueFrom(
+        this.authClient.send('auth.validate-token', { token }),
+      );
+
+      if (!response || !response.isValid) {
+        throw new UnauthorizedException('Token inválido o expirado');
+      }
+
+      request.email = response.email;
+      return true;
+    } catch {
+      throw new UnauthorizedException('No autorizado');
+    }
+  }
+}
--- src/post/post.controller.ts
+++ src/post/post.controller.ts
@@ -6,16 +6,19 @@ import {
   Param,
   Patch,
   Post,
+  UseGuards,
 } from '@nestjs/common';
 import { PostService } from './post.service';
 import { CreatePostDto } from './dto/create-post.dto';
 import { UpdatePostDto } from './dto/update-post.dto';
+import { AuthGuard } from 'src/auth/guard/auth.guard';
 
-@Controller()
+@Controller('post')
 export class PostController {
   constructor(private readonly postService: PostService) {}
 
   @Post()
+  @UseGuards(AuthGuard)
   create(@Body() createPostDto: CreatePostDto) {
     return this.postService.create(createPostDto);
   }
@@ -26,16 +29,19 @@ export class PostController {
   }
 
   @Get(':id')
+  @UseGuards(AuthGuard)
   findOne(@Param('id') id: string) {
     return this.postService.findOne(id);
   }
 
   @Patch(':id')
+  @UseGuards(AuthGuard)
   update(@Param('id') id: string, @Body() updatePostDto: UpdatePostDto) {
     return this.postService.update(id, updatePostDto);
   }
 
   @Delete(':id')
+  @UseGuards(AuthGuard)
   remove(@Param('id') id: string) {
     return this.postService.remove(id);
   }
